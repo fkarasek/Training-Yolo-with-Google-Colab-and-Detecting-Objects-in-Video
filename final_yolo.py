0a918d3ef2dbdb4f4b778bbd889f344a3ae5306f
fkarasek <50097996+fkarasek@users.noreply.github.com>
2019-04-29 17:11:21 +0900

    Add files via upload

---
+++ final_yolo.py
@@ -0,0 +1,163 @@
+# Example script for command line: python3 final_yolo.py --video=sample.mp4
+
+import cv2 as cv
+import argparse
+import sys
+import numpy as np
+import os.path
+
+# Initialize the parameters
+confThreshold = 0.2  #Confidence threshold
+nmsThreshold = 0.1   #Non-maximum suppression threshold
+inpWidth = 416       #Width of network's input image (1920)
+inpHeight = 416      #Height of network's input image (1080)
+
+parser = argparse.ArgumentParser(description='Object Detection using YOLO in OPENCV')
+parser.add_argument('--image', help='Path to image file.')
+parser.add_argument('--video', help='Path to video file.')
+args = parser.parse_args()
+        
+# Load names of classes
+classesFile = "classes.names"
+classes = None
+with open(classesFile, 'rt') as f:
+    classes = f.read().rstrip('\n').split('\n')
+
+# Give the configuration and weight files for the model and load the network using them.
+modelConfiguration = "yolov3.cfg"
+modelWeights = "yolov3.backup"
+
+net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+# Get the names of the output layers
+def getOutputsNames(net):
+    # Get the names of all the layers in the network
+    layersNames = net.getLayerNames()
+    # Get the names of the output layers, i.e. the layers with unconnected outputs
+    return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# Draw the predicted bounding box
+def drawPred(classId, conf, left, top, right, bottom):
+    # Draw a bounding box.
+    cv.rectangle(frame, (left, top), (right, bottom), (200, 200, 0), 2)
+    
+    label = '%.2f' % conf
+        
+    # Get the label for the class name and its confidence
+    if classes:
+        assert(classId < len(classes))
+        label = '%s:%s' % (classes[classId], label)
+
+    cv.putText(frame, label, (left+10, top-5), cv.FONT_HERSHEY_SIMPLEX, 0.8, (200,200,0), 2)
+
+
+
+
+
+# Remove the bounding boxes with low confidence using non-maxima suppression
+def postprocess(frame, outs):
+    frameHeight = frame.shape[0]
+    frameWidth = frame.shape[1]
+
+    # Scan through all the bounding boxes output from the network and keep only the
+    # ones with high confidence scores. Assign the box's class label as the class with the highest score.
+    classIds = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                center_x = float(detection[0] * frameWidth)
+                center_y = float(detection[1] * frameHeight)
+                width = float(detection[2] * frameWidth)
+                height = float(detection[3] * frameHeight)
+                left = float(center_x - width / 2)
+                top = float(center_y - height / 2)
+                classIds.append(classId)
+                confidences.append(float(confidence))
+                boxes.append([left, top, width, height])
+
+    # Perform non maximum suppression to eliminate redundant overlapping boxes with
+    # lower confidences.
+    indices = cv.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        left = float(box[0])
+        top = float(box[1])
+        width = float(box[2])
+        height = float(box[3])
+        drawPred(classIds[i], confidences[i], int(left*20/19), int(top*10/9), int((left + width)*20/19), int((top + height)*10/9))
+
+
+# Process inputs
+winName = 'Deep learning object detection in OpenCV'
+cv.namedWindow(winName, cv.WINDOW_NORMAL)
+
+outputFile = "yolo_out_py.avi"
+if (args.image):
+    # Open the image file
+    if not os.path.isfile(args.image):
+        print("Input image file ", args.image, " doesn't exist")
+        sys.exit(1)
+    cap = cv.VideoCapture(args.image)
+    outputFile = args.image[:-4]+'_yolo_out_py.jpg'
+elif (args.video):
+    # Open the video file
+    if not os.path.isfile(args.video):
+        print("Input video file ", args.video, " doesn't exist")
+        sys.exit(1)
+    cap = cv.VideoCapture(args.video)
+    outputFile = args.video[:-4]+'_yolo_out_py.avi'
+else:
+    # Webcam input
+    cap = cv.VideoCapture(0)
+
+# Get the video writer initialized to save the output video
+if (not args.image):
+    vid_writer = cv.VideoWriter(outputFile, cv.VideoWriter_fourcc('M','J','P','G'), 30, (round(cap.get(cv.CAP_PROP_FRAME_WIDTH)),round(cap.get(cv.CAP_PROP_FRAME_HEIGHT))))
+
+while cv.waitKey(1) < 0:
+    
+    # get frame from the video
+    hasFrame, frame = cap.read()
+    
+    # Stop the program if reached end of video
+    if not hasFrame:
+        print("Done processing")
+        print("Output file is stored as ", outputFile)
+        cv.waitKey(3000)
+        # Release device
+        cap.release()
+        break
+
+    # Create a 4D blob from a frame.
+    blob = cv.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
+
+    # Sets the input to the network
+    net.setInput(blob)
+
+    # Runs the forward pass to get output of the output layers
+    outs = net.forward(getOutputsNames(net))
+
+    # Remove the bounding boxes with low confidence
+    postprocess(frame, outs)
+
+    # Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
+    t, _ = net.getPerfProfile()
+    label = 'Inference time: %.2f ms' % (t * 1000.0 / cv.getTickFrequency())
+    cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+
+    # Write the frame with the detection boxes
+    if (args.image):
+        cv.imwrite(outputFile, frame.astype(np.uint8))
+    else:
+        vid_writer.write(frame.astype(np.uint8))
+
+    cv.imshow(winName, frame)
+
